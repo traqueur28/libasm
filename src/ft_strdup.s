f13155e3085d56d1af27d0af8a7f2aff090f2627
Jarod GOURLIN <jgourlin@paul-f4Ar5s11.clusters.42paris.fr>
2023-06-27 03:52:05 +0200

    strdup done

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -1,8 +1,36 @@
 bits    64
 
+extern  ft_strlen
+extern  ft_strcpy
+extern  malloc
+extern  __errno_location
+
 section .text
 
 global  ft_strdup
 
+
+;   The strdup() function returns a pointer to a new string which is a duplicate of the string s.
+;   Memory for the new string is obtained with malloc(3), and can be freed with free(3).
+
+;   rdi = string to cpy
 ft_strdup:
+    call    ft_strlen   ; get size string
+    push    rdi         ; store rdi on stack
+    inc rax
+    mov rdi, rax        ; store size in rdi
+    call    malloc      ; rax = malloc(rdi)
+    test    rax, rax    ; check if error
+    js  error           ;
+    pop rsi             ; get src
+    mov rdi, rax        
+    call    ft_strcpy   ; rax = ft_strcpy(rdi, rsi)
+    ret
+
+error:
+    neg		rax                 ; change rax to positive
+	mov		rdx, rax            ; store rax in rdi
+	call	__errno_location    ; change errno to
+	mov		qword [rax], rdx    ; set errno
+	mov		rax, 0              ; return 0
     ret
